f9d4956238964ba31cacb84ee88e21976dd22430
Jay Morelli <jcrmorelli@gmail.com>
2021-04-22 07:48:56 +0100

    Criar/Salvar Logistica - Use Case

--- src/Evento/forms.py
+++ src/Evento/forms.py
@@ -1,20 +1,33 @@
 from django import forms
 
 from django.contrib.admin.widgets import AdminDateWidget, AdminTimeWidget, AdminSplitDateTime
-from django.forms.widgets import SelectDateWidget
 
-from .models import Evento
+from .models import Evento, Logistica
+
 
 class EventoForm(forms.ModelForm):
     data_i=forms.DateField(widget = AdminDateWidget())
     hora_i = forms.TimeField(widget = AdminTimeWidget())
     data_f=forms.DateField(widget = AdminDateWidget())
     hora_f = forms.TimeField(widget = AdminTimeWidget())
+
     class Meta:
         model = Evento 
         fields = [
             'nome',
             'descricaogeral',
             'maxparticipantes',
+        ]
+
+
+class LogisticaForm(forms.ModelForm):
+    data_i=forms.DateField(widget = AdminDateWidget())
+    hora_i = forms.TimeField(widget = AdminTimeWidget())
+    data_f=forms.DateField(widget = AdminDateWidget())
+    hora_f = forms.TimeField(widget = AdminTimeWidget())
+    class Meta:
+        model = Logistica
+        fields = [
+            'nome',
         ]
 
--- src/Evento/views.py
+++ src/Evento/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from .forms import EventoForm
+from .forms import EventoForm, LogisticaForm
 from Neglected.models import Timedate
-from .models import Evento
+from .models import Evento, Logistica
+from Recurso.models import Tipodeequipamento, Tipoespaco, Tiposervico
 from django.urls import reverse
 
 
@@ -16,17 +17,66 @@ def home_view(request):
 def eventos(request):
     #  events = Evento.objects.all().filter(estado='aceite')
     events = Evento.objects.all()  # temporary
+    logistica = Logistica.objects.all()
 
     context = {
         'eventos': events,
+        'logistica': logistica
     }
 
     return render(request, 'Evento/eventos.html', context)
 
 
 def create_event2(request, event_id):
-    print(event_id)
-    return render(request, 'Evento/criar_evento2.html')
+    evento = Evento.objects.get(id=event_id)
+    form = LogisticaForm(request.POST or None)
+    if form.is_valid():
+        nome = request.POST.get("nome")
+        quantidade = request.POST.get("quantidade")
+        tipo = request.POST.get("logistica")
+
+        # Date and time data
+        date_i = request.POST.get("data_i")
+        date_f = request.POST.get("data_f")
+        time_i = request.POST.get("hora_i")
+        time_f = request.POST.get("hora_f")
+        # Create TimeDate Obj
+        horario = Timedate(datainicial=date_i, horainicial=time_i,
+                           datafinal=date_f, horafinal=time_f)
+        horario.save()
+
+        # Create Logistica and TipoLogistica
+        logistica = Logistica(nome=nome, eventoid=evento)
+        logistica.save()
+        if tipo == "equipamento":
+            tipo_equipamento = Tipodeequipamento(nome=nome, quantidade=quantidade, logisticaid=logistica,
+                                                 horariorequisicao=horario)
+            tipo_equipamento.save()
+        elif tipo == "servico":
+            tipo_servico = Tiposervico(nome=nome, quantidade=quantidade, logisticaid=logistica,
+                                                 horariorequisicao=horario)
+            tipo_servico.save()
+        elif tipo == "espaco":
+            tipo_espaco = Tipoespaco(nome=nome, quantidade=quantidade, logisticaid=logistica,
+                                                 horariorequisicao=horario)
+            tipo_espaco.save()
+        else:
+            exit(0)
+
+        # Update Evento obj state
+        evento.estado = 'Logistica Pendente'
+        evento.save()
+
+        # Redirect to eventos page
+        return redirect('Evento:eventos')
+
+    else:
+        print("Not Valid!")
+    context = {
+        'evento': evento,
+        'form': form
+    }
+    return render(request, 'Evento/criar_evento2.html', context)
 
 
 def create_event(request):
@@ -46,9 +96,10 @@ def create_event(request):
         nome = request.POST.get("nome")
         desc = request.POST.get("descricaogeral")
         max_p = request.POST.get("maxparticipantes")
+        val_insc = 1 if request.POST.get("radio") == 'sim' else 0
         # Create Evento Obj
         evento = Evento(nome=nome, descricaogeral=desc, maxparticipantes=max_p,
-                        horario=horario, estado="Pendente")
+                        horario=horario, estado="Pendente", val_inscritos=val_insc, inscritos=0)
         evento.save()
         return redirect('Evento:eventos')
     else:
