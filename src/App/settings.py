fbdf0190eaa62c6b554b771587095e58631da709
Ana Rocha <46908402+AnaLuciaRocha@users.noreply.github.com>
2021-04-29 10:14:32 +0100

    Create form (#1)
    * now we can see template on formulario
    
    * first commit

--- src/App/settings.py
+++ src/App/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     # Custom
 
     'Evento',
-    'GestorTemplates',
+    'GestorTemplates.apps.GestortemplatesConfig',
     'Inscricao',
     'Notificacao',
     'Recurso',
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'App.wsgi.application'
 DATABASES = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
