8159a2c322dfabfc36e4b2ee4266fd924d4e9e4e
Ricardo Correia <rjcorreia12@gmail.com>
2021-06-10 17:38:51 +0100

    Merged with RicJay-1

--- src/Recurso/models.py
+++ src/Recurso/models.py
@@ -232,7 +232,7 @@ class Tipodeequipamento(models.Model):
 
 
 class TimedateRecurso(models.Model):
-    id = models.AutoField(db_column='Id', primary_key=True)  # Field name made lowercase.
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
     timedateid = models.OneToOneField(Timedate, models.DO_NOTHING, db_column='TimeDateID')  # Field name made lowercase.
     recursoid = models.OneToOneField(Recurso, models.DO_NOTHING, db_column='RecursoID')  # Field name made lowercase.
 
